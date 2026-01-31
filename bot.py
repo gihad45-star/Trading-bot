12c5c066417f22e507c20a087d45a360ad2852e1
gihad45-star <gihad45@gmail.com>
2026-01-31 17:58:59 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,11 @@
-print("Bot is running")
+import time
+
+print("Bot started...")
+
+while True:
+    print("Waiting for signal...")
+    time.sleep(5) 
+  print("bot is running")
+                  
+    
+                      
